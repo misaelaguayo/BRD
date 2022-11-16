468db533d69f4814e4276501ef7317c5a0ac6e29
Misael Aguayo <aguayo.misael@gmail.com>
2022-11-15 18:13:01 -0600

    printing out each token individually

---
+++ Lox.py
@@ -0,0 +1,28 @@
+import sys
+
+class Lox:
+    def run(self, source: str) -> None:
+        tokens = source.split(" ")
+        for token in tokens:
+            print(token)
+
+    def runPrompt(self):
+        while True:
+            line = input("> ")
+            if not line:
+                break
+            self.run(line)
+
+    def runFile(self, path: str) -> None:
+        raise NotImplementedError("Has not been implemented yet!")
+
+    def __init__(self) -> None:
+        if len(sys.argv) > 2:
+            raise Exception("Usage:plox [script]")
+            return
+        elif len(sys.argv) == 2:
+            self.runFile(sys.argv[0])
+        else:
+            self.runPrompt();
+
+Lox()
